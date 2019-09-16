26421caa51b8fb9296110456fcddf0e5f5bf6c5c
Unknown <nikola-dedo@yandex.ru>
2019-09-16 21:51:46 +0700

    add adaptive footer and 404

--- js/frontend/common.js
+++ js/frontend/common.js
@@ -1,10 +1,12 @@
 import SimpleBar from 'simplebar';
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   let mobileMenu = document.querySelector('.js-mobile-menu');
   let headerBlock = document.querySelector('.b-header');
+  let btnShowPlayers = document.querySelectorAll('.js-show-player');
+  let btnClosePlayers = document.querySelectorAll('.js-close-player');
 
-  mobileMenu.addEventListener('click', function () {
+  mobileMenu.addEventListener('click', function() {
     headerBlock.classList.toggle('active')
   });
 
@@ -13,4 +15,33 @@ document.addEventListener('DOMContentLoaded', function () {
   if (scrollbar) {
     const simpleBar = new SimpleBar(scrollbar);
   }
+
+  [...btnShowPlayers].forEach((button) => {
+    button.addEventListener('click', () => {
+      button.nextElementSibling.classList.add('b-teams__player-box--show')
+    });
+  });
+  [...btnClosePlayers].forEach((button) => {
+    button.addEventListener('click', () => {
+      button.parentNode.classList.remove('b-teams__player-box--show')
+    });
+  });
+
+  function clickOutside(e) {
+    if (e.target.classList.contains('js-show-player')) {
+      return
+    }
+
+    let boxs = document.querySelectorAll('.b-teams__player-box');
+
+    [...boxs].forEach((box) => {
+      const inside = box.contains(e.target);
+      if (!inside) {
+        box.classList.remove('b-teams__player-box--show')
+      }
+    });
+  }
+
+  document.addEventListener('click', clickOutside, false);
 });
+
