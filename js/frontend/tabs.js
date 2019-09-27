72650c9a0c8cb890851d9fae20370e7a20777357
Unknown <nikola-dedo@yandex.ru>
2019-09-28 00:06:08 +0700

    table and home page

--- js/frontend/tabs.js
+++ js/frontend/tabs.js
@@ -1,57 +1,49 @@
 // require('jquery-mousewheel');
 // require('malihu-custom-scrollbar-plugin');
 
+// $(document).ready(function () {
+//   let linkTab = $('.js-tab-item > div');
+//   let elClosest = '.js-tab';
+//
+//   linkTab.on('click', function (e) {
+//     let $this = $(this);
+//     let tabActive = $this.closest(elClosest).find('.js-tab-item');
+//     let contentTab = $this.closest(elClosest).find('.js-tab-contents .js-tab-content');
+//
+//     tabActive.find('div').removeClass('tab__item--active');
+//     $this.addClass('tab__item--active');
+//     let clickedTab = tabActive.find('.tab__item--active');
+//
+//     contentTab.removeClass('active');
+//     let clickedTabIndex = clickedTab.index();
+//
+//     contentTab.eq(clickedTabIndex).addClass('active');
+//   });
+//
+// });
+
 $(document).ready(function () {
-  let linkTab = $('.js-tab-item > div');
-  let elClosest = '.js-tab';
+  let linkTab = $('.js-tabs-items').children();
+  let elClosest = '.js-tabs-container';
 
   linkTab.on('click', function (e) {
     let $this = $(this);
-    let tabActive = $this.closest(elClosest).find('.js-tab-item');
-    let contentTab = $this.closest(elClosest).find('.js-tab-contents .js-tab-content');
+    let tabActive = $this.closest(elClosest).find('.js-tabs-items');
+    let contentTab = $this.closest(elClosest).find('.js-tabs-contents').children();
+    let isAllVersion = tabActive.find('.is-all')[0];
 
-    tabActive.find('div').removeClass('tab__item--active');
-    $this.addClass('tab__item--active');
-    let clickedTab = tabActive.find('.tab__item--active');
+    tabActive.children().removeClass('is-active');
+    $this.addClass('is-active');
+    let clickedTab = tabActive.find('.is-active');
+    let isAllTab = $this.hasClass('is-all');
 
-    contentTab.removeClass('active');
-    let clickedTabIndex = clickedTab.index();
+    contentTab.removeClass('is-active');
+    if (!isAllTab) {
+      let clickedTabIndex = isAllVersion ? clickedTab.index() - 1 : clickedTab.index();
 
-    contentTab.eq(clickedTabIndex).addClass('active');
+      contentTab.eq(clickedTabIndex).addClass('is-active');
+    } else {
+      contentTab.addClass('is-active');
+    }
   });
-
-  // let linkTabInner = $('.js-tab-item-inner > div');
-  // let elClosestInner = '.js-tab-inner';
-  //
-  // linkTabInner.on('click', function (e) {
-  //   let $this = $(this);
-  //   let tabActive = $this.closest(elClosestInner).find('.js-tab-item-inner');
-  //   let contentTab = $this.closest(elClosestInner).find('.js-tab-content-inner > div.b-main-tab__body-wrap');
-  //
-  //   tabActive.find('div').removeClass('active');
-  //   $this.addClass('active');
-  //   let clickedTab = tabActive.find('.active');
-  //
-  //   contentTab.removeClass('active');
-  //   let clickedTabIndex = clickedTab.index();
-  //
-  //   contentTab.eq(clickedTabIndex).addClass('active');
-  // });
-  //
-  // let $link = $('.js-table-tabs > button');
-  //
-  // $link.on('click', function() {
-  //
-  //   let $this = $(this);
-  //   let $links = $this.parent().find('button');
-  //   $('.js-scrollbar-table').mCustomScrollbar("scrollTo","left");
-  //   $links.removeClass('active');
-  //   $this.addClass('active');
-  //
-  //   let $contents = $this.parent().parent().find('.b-custom-table__wrap');
-  //   let $current = $this.parent().parent().find(`[data-content=${$this.data('anchor')}]`);
-  //
-  //   $contents.removeClass('active');
-  //   $current.addClass('active');
-  // });
 });
